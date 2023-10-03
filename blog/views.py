33e5adb69604fcd7a698ee532074e2804a7810f8
isabellaobrien <bellaobrien.it@gmail.com>
2023-10-03 06:29:09 +0000

    Create post model

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+# def home(request):
+#     return render(request, 'index.html', {})
 
-# Create your views here.
+class Home(ListView):
+    model = Post
+    template_name = 'index.html'
