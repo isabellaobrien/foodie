33faa74decaf2666df5e3d3b3e841a1a8e87d806
isabellaobrien <bellaobrien.it@gmail.com>
2023-10-11 18:38:16 +0000

    Add change password

--- members/urls.py
+++ members/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import UserRegistration
-
+from .views import UserRegistration, UpdateProfile, ChangePassword
+from django.contrib.auth import views as auth_views
+from . import views
 urlpatterns = [
     path('register/', UserRegistration.as_view(), name="register"),
+    path('edit_profile/', UpdateProfile.as_view(), name="edit_profile"),
+    # path('password/', auth_views.PasswordChangeView.as_view(template_name="registration/change-password.html")),
+    path('password/', ChangePassword.as_view(template_name="registration/change-password.html")),
+    path('password_success/', views.password_success, name="password_success"),
+    
 ]
